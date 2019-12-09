19378300c28a3eefc211303bbd5bdf363cc09957
shanelonergan <sptlonergan@gmail.com>
2019-12-09 10:32:42 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
-// const symDiff = (arr1, arr2) => {
-//   let unique1 = arr1.filter(item => arr2.indexOf(item) === -1)
-//   let unique2 = arr2.filter(item => arr1.indexOf(item) === -1)
-
-//   const uniqueArr = unique1.concat(unique2)
-//   console.log(uniqueArr)
-//   return uniqueArr
-// }
-
-const symDiff = (arr1, arr2) => [
-  ...arr1.filter(item => !arr2.includes(item)),
-  ...arr2.filter(item => !arr1.includes(item))
-];
+const symDiff = (arr1, arr2) => {
+  console.log(...arr2.filter(item => !arr1.includes(item)))
+  return [
+    ...arr1.filter(item => !arr2.includes(item)),
+    ...arr2.filter(item => !arr1.includes(item))
+  ]
+};
 
 const sym = (...args) => {
-  [...new Set(args.reduce(symDiff))]
+  console.log([...new Set(args.reduce(symDiff))])
+  return [...new Set(args.reduce(symDiff))]
 }
 
 sym([1, 2, 3], [5, 2, 1, 4]);
