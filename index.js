f123eedce1b7514df4471521b22e5239bffbe73b
shanelonergan <sptlonergan@gmail.com>
2019-12-08 20:41:04 -0500

    1 file modified

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 function sym(args) {
-  console.log([...args]);
+  const argArr = Array.prototype.slice.call(arguments)
+  console.log(argArr)
 }
 
 sym([1, 2, 3], [5, 2, 1, 4]);
