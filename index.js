83c154bee66e1148a2df21b27da0b26246d8e221
shanelonergan <sptlonergan@gmail.com>
2019-12-08 20:48:40 -0500

    1 file modified

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+const symDiff = (arr1, arr2) => {
+  unique1 = arr1.filter(item => arr2.indexOf(item) === -1)
+
+}
+
 function sym(args) {
   const argArr = Array.prototype.slice.call(arguments)
-  console.log(argArr)
+  
+  
 }
 
 sym([1, 2, 3], [5, 2, 1, 4]);
