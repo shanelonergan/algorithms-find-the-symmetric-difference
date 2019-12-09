dc8c6b96999afe658b886c34ef3c75b3364ba855
shanelonergan <sptlonergan@gmail.com>
2019-12-08 21:28:14 -0500

    1 file modified

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const symDiff = (arr1, arr2) => {
-  let unique1 = arr1.filter(item => arr2.indexOf(item) === -1)
-  let unique2 = arr2.filter(item => arr1.indexOf(item) === -1)
+// const symDiff = (arr1, arr2) => {
+//   let unique1 = arr1.filter(item => arr2.indexOf(item) === -1)
+//   let unique2 = arr2.filter(item => arr1.indexOf(item) === -1)
 
-  const uniqueArr = unique1.concat(unique2)
-  console.log(uniqueArr)
-  return uniqueArr
-}
+//   const uniqueArr = unique1.concat(unique2)
+//   console.log(uniqueArr)
+//   return uniqueArr
+// }
 
-symDiff([1, 2, 3], [5, 2, 1, 4]) // working
+const symDiff = (arr1, arr2) => [
+  ...arr1.filter(item => !arr2.includes(item)),
+  ...arr2.filter(item => !arr1.includes(item))
+];
 
-function sym(args) {
-  const argArr = [].slice.call(arguments)
-
-  console.log(argArr)
-  
-  args.reduce(symDiff)
+const sym = (...args) => {
+  [...new Set(args.reduce(symDiff))]
 }
 
 sym([1, 2, 3], [5, 2, 1, 4]);
