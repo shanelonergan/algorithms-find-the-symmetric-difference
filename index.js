67d83771ca7ca254cc070b58414a01640196df6f
shanelonergan <sptlonergan@gmail.com>
2019-12-08 21:00:43 -0500

    1 file modified

--- index.js
+++ index.js
@@ -18,8 +18,12 @@ symDiff([1, 2, 3], [5, 2, 1, 4]) // working
 
 function sym(args) {
   const argArr = Array.prototype.slice.call(arguments)
+
+  console.log(argArr)
   
-  
+  const argLength = argArr.length
+
+  console.log(argLength)
 }
 
 sym([1, 2, 3], [5, 2, 1, 4]);
